200a57ca32b8f300a49c895a7613e9e34a878bcb
Uchio Kondo <udzura@udzura.jp>
2019-06-13 15:24:46 +0900

    Implement brain, scorekeeper

--- lib/daimyo_bot.rb
+++ lib/daimyo_bot.rb
@@ -1,8 +1,20 @@
 # coding: utf-8
 require 'slack-ruby-bot'
 require 'async'
+require 'redis'
+require 'redis-namespace'
+
+Redis.current = Redis.new(url: (ENV["REDIS_URL"] ||= 'redis://localhost:6379'))
 
 class DaimyoBot < SlackRubyBot::Bot
+  def self.brain
+    @brain ||= Redis::Namespace.new(:daimyo_brain, redis: Redis.current)
+  end
+
+  def self.scoreboard
+    @scoreboard ||= Redis::Namespace.new(:daimyo_scoreboard, redis: Redis.current)
+  end
+
   help do
     title 'Daimyo Bot'
     desc '大名エンジニアカレッジのボット.'
@@ -22,5 +34,21 @@ class DaimyoBot < SlackRubyBot::Bot
 
   match /(大名小|FGN)(の場所)?/ do |client, data, match|
     client.say(text: 'here: https://goo.gl/maps/tkQ1sNnLJp6MPYtx6', channel: data.channel)
+  end
+
+  match /([^\s+-]+)\+\+/ do |client, data, match|
+    user = match[1]
+    score = scoreboard.get(user) || 0
+    score = score.to_i + 1
+    scoreboard.set(user, score.to_s)
+    client.say(text: "Incremented #{user}: #{score}pt", channel: data.channel)
+  end
+
+  match /([^\s+-]+)--/ do |client, data, match|
+    user = match[1]
+    score = scoreboard.get(user) || 0
+    score = [0, score.to_i - 1].max
+    scoreboard.set(user, score.to_s)
+    client.say(text: "Decremented #{user}: #{score}pt", channel: data.channel)
   end
 end
